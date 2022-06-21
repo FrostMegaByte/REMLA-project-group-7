c10f95e0835e5c14a634e5c9b5aa8ad798227d0c
Hielke Walinga <hielkewalinga@gmail.com>
2022-06-21 10:00:11 +0200

    add fastapi test

---
+++ tests/test_http_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue 21 Jun 2022 09:53:20 AM CEST
+
+@author hielke
+"""
+
+from fastapi.testclient import TestClient
+
+from src.serve_model import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/metrics-prometheus")
+    assert response.status_code == 200
